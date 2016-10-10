828903334b97953cd49cad3a0a9267613a3719cf
Metsola Waltteri J S <wame@da3-cs-dk110-02.helsinki.fi>
2016-10-10 11:24:31 +0300

    tadaa

---
+++ src/main/java/tikape/runko/database/AihealueDao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.domain.Aihealue;
+/**
+ *
+ * @author wame
+ */
+public class AihealueDao implements Dao<Aihealue, Integer> {
+
+    @Override
+    public Aihealue findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Aihealue> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
