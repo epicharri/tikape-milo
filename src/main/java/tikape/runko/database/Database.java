94dddaaeeea0500ce8b991d65f6b1c8423deef2b
epicharri <harri.kahkonen3@gmail.com>
2016-10-28 21:24:03 +0300

    muokkauksia postgres-lauseisiin

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -70,19 +70,18 @@ public class Database {
         //lista.add("DROP TABLE Viestiketju");
         //lista.add("DROP TABLE Aihealue");
         //alta poistettu id integer SERIAL PRIMARY KEY -lauseista integer.
-        lista.add("CREATE TABLE Viesti (\n"
-                + "id SERIAL PRIMARY KEY, \n"
-                + "aika datetime NOT NULL, \n"
-                + "nimimerkki varchar(20) NOT NULL, \n"
-                + "sisalto varchar(300) NOT NULL, \n"
-                + "viestiketju, FOREIGN KEY(viestiketju) REFERENCES Viestiketju(id));");
-        lista.add("CREATE TABLE Viestiketju (\n"
-                + "id SERIAL PRIMARY KEY, \n"
-                + "otsikko varchar(30) NOT NULL,\n"
-                + "aihealue, \n"
-                + "FOREIGN KEY(aihealue) REFERENCES Aihealue(id));");
-        lista.add("CREATE TABLE Aihealue (\n"
-                + "id SERIAL PRIMARY KEY, \n"
+        lista.add("CREATE TABLE Viesti ("
+                + "id SERIAL PRIMARY KEY, "
+                + "aika datetime NOT NULL, "
+                + "nimimerkki varchar(20) NOT NULL, "
+                + "sisalto varchar(300) NOT NULL, "
+                + "viestiketju integer REFERENCES Viestiketju(id));");
+        lista.add("CREATE TABLE Viestiketju ("
+                + "id SERIAL PRIMARY KEY, "
+                + "otsikko varchar(30) NOT NULL,"
+                + "aihealue integer REFERENCES Aihealue(id));");
+        lista.add("CREATE TABLE Aihealue ("
+                + "id SERIAL PRIMARY KEY, "
                 + "nimi varchar(30) NOT NULL);");
         lista.add("INSERT INTO Aihealue(nimi) VALUES ('Koirat');");
         lista.add("INSERT INTO Aihealue(nimi) VALUES ('Kissat');");
